09e18997659dffb82c28406a8c6ba6837f6e5723
sergio <sergio>
2017-06-16 13:04:25 -0500

    FIRST API restful, sql select responde a postman & console

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('sql.db');
-db.run("INSERT into tareas VALUES(null,'tercer tarea.. desde node')");
-db.close();
+
+var express = require('express');
+var restapi = express();
+
+restapi.get('/data', function(req, res){
+    db.all("SELECT descr FROM tareas", function(err, rows){
+        res.json(rows);
+		console.log(rows);
+    });
+});
+
+restapi.listen(3000);
+console.log("Submit GET or POST to http://localhost:3000/data");
+//db.close();
