99a3168f54d93c8f7c49853548682e326644ef39
asecomsc <asecomsc@hotmail.com>
2019-01-31 00:31:13 -0600

    THE ONE, after serveral changes: add,del,move just use UPDATE

--- public/todo.js
+++ public/todo.js
@@ -1,13 +1,10 @@
 function fnSave() { 
-	if (typeof(miObj) !== "undefined") {miObj.remove();} // need to save for every <li>.click
 	var miarr = [];
     $("li").each(function(){
         miarr.push($(this).text());
     });	
-	$.ajax({
-		type:'POST', url:'http://192.168.0.2:3000/data',
-	    data:JSON.stringify({ elArr: miarr }), contentType:"application/json"
-	});
+	$.ajax({ type:'POST', url:'http://192.168.0.2:3000/data',
+	         data:JSON.stringify({ elArr: miarr }), contentType:"application/json" });
 }
 
 function miLoad() {
@@ -15,20 +12,15 @@ function miLoad() {
 		for (var key in data) {
 			var newLi = $('<li>' + data[key].descr + '</li>');
 			newLi.on('click', function() {
-				miObj = $(this);
-				$(this).fadeOut(1000);
+				    $(this).fadeOut(1000, function(){
+					   $(this).remove();
+					});
 			});
 			$('ol').append(newLi); 		
 		}
 	});
 }
 
-function mos() {  // mostrar, esta funcion es para ser llamada desde chrome-console
-	$('li').each(function(){
-   console.log($(this).text());
-	})    
-	console.log('----------');
-}
 $(document).ready(function() {
 	$(document).on('keypress',function(e) {
 		if(e.which == 13) {
@@ -36,8 +28,9 @@ $(document).ready(function() {
 				var newTask = $('#task').val();
 				var newLi = $('<li>' + newTask + '</li>');
 				newLi.on('click', function() {
-					miObj = $(this);
-					$(this).fadeOut(1000);
+				    $(this).fadeOut(1000, function(){
+					   $(this).remove();
+					});
 				});
 				$('ol').append(newLi); 
 				$('#task').val('');
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
-# USO DE ESTA VERSION
-  * update after several: new-tasks, moves 
-  * but need update after every delete
+# USO DE ESTA VERSION  "THE ONE"
+  * after several: add, del, move ya puedo usar SAVE
+
 
 
